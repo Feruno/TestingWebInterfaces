c8c935255f6600e4fc6d0b2df4bde2dda34fc49f
Александр Бондаренко <nice.alexsandr@mail.ru>
2023-08-10 23:36:13 +0300

    fix 5

--- src/test/java/ru/netology/appOrderTest.java
+++ src/test/java/ru/netology/appOrderTest.java
@@ -18,11 +18,12 @@ public class appOrderTest {
     private WebDriver driver;
 
 
+    /*
     @BeforeAll
     static void setUpAll() {
         System.setProperty("webdriver.chrome.driver", "driver/win/chromedriver.exe");
         //WebDriverManager.chromedriver().setup();
-    }
+    }*/
 
 
     @BeforeEach
